13f3491536bc7d2f95b26879971ff316e6cc5b13
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-22 19:33:57 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext.commands import Bot
 import asyncio
 import os
 
-bot = commands.Bot(command_prefix='claire ')
+bot = commands.Bot(command_prefix='aqua ')
 ownerID = "329337654850093056"
 
 # To remove the help command and make your own help command
